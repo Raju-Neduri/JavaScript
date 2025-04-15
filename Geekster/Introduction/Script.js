b0e02bcfe705d85c9ff0469ab45156538efec065
Neduri Raju <rajneduri123@gmail.com>
2025-04-15 18:21:15 +0530

    arthemetic operators

--- Geekster/Introduction/Script.js
+++ Geekster/Introduction/Script.js
@@ -1,2 +1,15 @@
-var a = [1, 2, 3, 4];
-console.log(a);
+// var c = [1, 2, 3, 4];
+// console.log(c);
+
+// var d = 25;
+// console.log(d);
+
+var a = 9;
+var b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
