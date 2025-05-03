52f2fe51bd920f54453e8730f915bf3fb85d1ad8
Neduri Raju <rajneduri123@gmail.com>
2025-05-03 17:33:09 +0530

    dom

---
+++ Geekster/Introduction to DOM/script.js
@@ -0,0 +1,4 @@
+// document.getElementsByClassName("heading2")[0].style.color = "red";
+document.getElementsByName("heading3")[0].style.color = "blue";
+const items = document.querySelectorAll(".heading2");
+items.forEach((item) => (item.style.color = "green"));
