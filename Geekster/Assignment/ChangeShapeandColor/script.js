d22436f864aa0751ec48b247289eddda04ad9c09
Neduri Raju <rajneduri123@gmail.com>
2025-05-06 20:29:48 +0530

    updated

---
+++ Geekster/Assignment/ChangeShapeandColor/script.js
@@ -0,0 +1,62 @@
+const circle = document.getElementById("circle");
+const inner = document.querySelector(".inner");
+const changeColorBtn = document.querySelector("#change-color button");
+const changeShapeBtn = document.querySelector("#change-shape button");
+
+const info = document.createElement("div");
+info.id = "info";
+document.getElementById("container").appendChild(info);
+
+const colors = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "pink",
+  "seagreen",
+  "gold",
+  "tomato",
+  "skyblue",
+];
+
+const shapes = [
+  { name: "Circle", borderRadius: "50%", width: "120px", height: "120px" },
+  { name: "Square", borderRadius: "0%", width: "120px", height: "120px" },
+  {
+    name: "Rounded Square",
+    borderRadius: "15px",
+    width: "120px",
+    height: "120px",
+  },
+  { name: "Capsule", borderRadius: "50px", width: "180px", height: "80px" },
+  {
+    name: "Ellipse",
+    borderRadius: "50% / 30%",
+    width: "160px",
+    height: "100px",
+  },
+];
+
+let shapeIndex = 0;
+let currentColor = circle.style.backgroundColor;
+
+function updateInfo() {
+  info.innerHTML = `<strong>Color:</strong> ${currentColor} <br><strong>Shape:</strong> ${shapes[shapeIndex].name}`;
+}
+
+changeColorBtn.addEventListener("click", () => {
+  currentColor = colors[Math.floor(Math.random() * colors.length)];
+  circle.style.backgroundColor = currentColor;
+  updateInfo();
+});
+
+changeShapeBtn.addEventListener("click", () => {
+  shapeIndex = (shapeIndex + 1) % shapes.length;
+  const shape = shapes[shapeIndex];
+  Object.assign(inner.style, shape);
+  updateInfo();
+});
+
+// Initial info display
+updateInfo();
