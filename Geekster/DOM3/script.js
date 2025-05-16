8380b56f6c6f20733896d898cf93ed82710cc4d4
Neduri Raju <rajneduri123@gmail.com>
2025-05-16 16:24:12 +0530

    dom 3

--- Geekster/DOM3/script.js
+++ Geekster/DOM3/script.js
@@ -1,10 +1,10 @@
 let createElement = document.createElement("div");
-createElement.style.height = "100px";
+createElement.style.height = "200px";
 createElement.style.width = "100px";
 createElement.style.backgroundColor = "blue";
 createElement.style.color = "black";
 createElement.style.textAlign = "center";
 createElement.style.fontSize = "30px";
-createElement.innerText = "this is new text added ";
+createElement.innerText = "this is new text added by Raj";
 
 document.body.appendChild(createElement); // Now this is valid
