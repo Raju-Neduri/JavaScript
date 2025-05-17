2788997fab56d1cde6d24171e6217d5b4ba57e8d
Neduri Raju <rajneduri123@gmail.com>
2025-05-18 02:07:07 +0530

    responsive and toggle added

--- Geekster/Emoji Project/script.js
+++ Geekster/Emoji Project/script.js
@@ -334,3 +334,23 @@ sortSelect.addEventListener("change", renderPlayers);
 
 // Initial render
 renderPlayers();
+
+//toggle
+const toggleBtn = document.getElementById("themeToggle");
+const body = document.body;
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+} else {
+  body.classList.add("light-mode");
+}
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  body.classList.toggle("light-mode");
+  localStorage.setItem(
+    "theme",
+    body.classList.contains("dark-mode") ? "dark" : "light"
+  );
+});
