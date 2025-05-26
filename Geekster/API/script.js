90a83963e609b741b2743fde7ac96baaa1cb1e91
Neduri Raju <rajneduri123@gmail.com>
2025-05-26 19:34:50 +0530

    update

--- Geekster/API/script.js
+++ Geekster/API/script.js
@@ -1,10 +1,17 @@
-fetch("https://reqres.in/api/users?page=2")
-  .then(function (res) {
-    return res.json(); // Convert response to JSON
-  })
-  .then(function (data) {
-    console.log(data); // Now you're logging the actual data!
-  })
-  .catch(function (error) {
-    console.log("Error:", error);
-  });
+// fetch('https://reqres.in/api/users?page=2')
+//   .then(function (res) {
+//     return res.json(); // Convert response to JSON
+//   })
+//   .then(function (data) {
+//     console.log(data); // Now you're logging the actual data!
+//   })
+//   .catch(function (error) {
+//     console.log("Error:", error);
+//   });
+
+async function makingrequest() {
+  let res = await fetch("https://reqres.in/api/users?page=2");
+  let data = await res.json();
+  console.log(data.data);
+}
+makingrequest();
